3b775659769509291f7cea4947bcb3c9b163c270
CHAGUER Reda <Reda.Chaguer@bordeaux-inp.fr>
2021-09-08 08:15:04 +0200

    Update generation script

--- src/generate-dataset.sh
+++ src/generate-dataset.sh
@@ -9,7 +9,7 @@ usage()
 # Generate a Number between 0 and 2^32-1 (RANDOM gives between 0 and 2^16-1)
 generate()
 {
-    echo "$(od -N 4  -t uL -An /dev/random | tr -d " ")"
+    echo "$(od -N 4  -t uL -An /dev/random | tr -d " ") $1" 
 }
 
 # Verify the number of arguments given
@@ -21,7 +21,8 @@ fi
 
 # Init Params
 filename=$1
-num_records=$2
+batch_size=1000
+num_records=$(($2 / $batch_size))
 
 # Delete the file if exists
 if [ -f $filename ]
@@ -32,8 +33,16 @@ fi
 # Create the file
 touch $filename
 
+string=$(pwgen 95 1)
+
 # Generate data
 for i in $(eval echo {1..$num_records})
 do
-    echo "$(generate) $(pwgen 95 1)" >> $filename
+    batch=
+    for i in $(eval echo {2..$batch_size})
+    do
+        batch+="$(generate $string)\n"
+    done
+    batch+="$(generate $string)"
+    echo -e $batch >> $filename
 done
